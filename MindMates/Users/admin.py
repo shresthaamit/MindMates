99c8b29d4b0d65d57464e3637633d220eb2103e0
AmitStha <shresthaamit273@gmail.com>
2024-09-16 10:58:12 +0545

    User model created and profile implemented.

--- MindMates/Users/admin.py
+++ MindMates/Users/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import UserProfile
 # Register your models here.
+class ProfileAdmin(admin.ModelAdmin):
+    readonly_fields = ('id',)
+    
+    
+admin.site.register(UserProfile, ProfileAdmin)
--- MindMates/Users/models.py
+++ MindMates/Users/models.py
@@ -1,3 +1,20 @@
+from typing import Any
 from django.db import models
-
+from django.contrib.auth.models import User
+import os
+from django.utils.deconstruct import deconstructible
 # Create your models here.
+@deconstructible
+class GenerateProfileImagePath(object):
+    def __init__(self):
+        pass
+    
+    def __call__(self, instance, filename):
+        ext = filename.split('.')[-1]
+        path = f'media/accounts/{instance.user.id}/images/'
+        name = f'profile_image.{ext}'
+        return os.path.join(path, name)
+USER_PROFILE_IMAGE_PATH = GenerateProfileImagePath()
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_picture = models.FileField(upload_to=USER_PROFILE_IMAGE_PATH, blank=True, null=True)
