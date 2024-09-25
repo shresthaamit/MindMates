73529288f94c004ecca88d79540fb9271b150d04
AmitStha <shresthaamit273@gmail.com>
2024-09-25 18:40:44 +0545

    Permission for useraccountedit and GetPost

---
+++ MindMates/Users/permissions.py
@@ -0,0 +1,13 @@
+from rest_framework import permissions
+
+class IsOwnerOrReadOnlyAndGetPost(permissions.BasePermission):
+    def has_permission(self, request,view):
+        return True
+    
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        if not request.user.is_anonymous:
+            return request.user == obj
+        
+        return False
--- MindMates/Users/views.py
+++ MindMates/Users/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from django.contrib.auth.models import User
 from .serializers import UserSerializer
+from .permissions import IsOwnerOrReadOnlyAndGetPost
 # Create your views here.
 class UserViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsOwnerOrReadOnlyAndGetPost]
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
