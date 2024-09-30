6c7636ea97c29f216fb5a9de241e91bdb022bf43
AmitStha <shresthaamit273@gmail.com>
2024-09-30 18:53:40 +0545

    Password Change added

--- MindMates/MindMates/urls.py
+++ MindMates/MindMates/urls.py
@@ -8,14 +8,14 @@ from Users import routers as users_urls
 from django.conf import settings
 
 auth_api_urls = [
-    path('auth/', include('oauth2_provider.urls', namespace='oauth2_provider')),  # OAuth2 provider URLs
+    path('auth/', include('oauth2_provider.urls', namespace='oauth2_provider')), 
 ]
 
 if settings.DEBUG:
-    auth_api_urls.append(path('verify/', include('rest_framework.urls')))  # Debug verification URLs
+    auth_api_urls.append(path('verify/', include('rest_framework.urls')))  
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("accounts/", include(users_urls.router.urls)),  # Custom user accounts API
+    path("accounts/", include(users_urls.router.urls)),  
     *auth_api_urls,  # OAuth2 URLs
 ]
--- MindMates/Users/serializers.py
+++ MindMates/Users/serializers.py
@@ -3,15 +3,30 @@ from rest_framework import serializers
 
 class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, required=False)
+    old_password = serializers.CharField(write_only=True, required=False)
     username = serializers.CharField(read_only=True)
     def create(self, validated_data):
         password = validated_data.pop("password")
         user = User.objects.create(**validated_data)
         user.set_password(password)
         user.save()
-        
         return user
+    
+    def update(self, instance, validated_data):
+        try:
+            user = instance
+            password = validated_data.pop("password")
+            old_password = validated_data.pop("old_password")
+            if user.check_password(old_password):
+                user.set_password(password)
+                
+            else:
+                raise Exception("Old password is invalid")
+            user.save()
+        except Exception as err:
+            raise serializers.ValidationError({"info":err})
         
+        return super(UserSerializer,self).update(instance,validated_data)
     class Meta:
         model = User
-        fields = ['url','id','username','email','first_name','last_name','password']
+        fields = ['url','id','username','email','first_name','last_name','password','old_password']
