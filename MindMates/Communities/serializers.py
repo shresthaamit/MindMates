e270dfdafc21ac8bfc43f2b1257dfec4981f9e04
Amitstha <shresthaamit273@gmail.com>
2025-04-20 14:19:02 +0545

    Communities started serializer created

--- MindMates/Communities/serializers.py
+++ MindMates/Communities/serializers.py
@@ -5,6 +5,21 @@ from rest_framework import serializers
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model =User
-        fields = ['name', 'email']
+        fields = ['id', 'username', 'email',]
+        
+        
+class CommunitySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Community
+        fields =['id','name','description','image']
+        extra_kwargs = {
+            'creaters':{'read_only':True}
+        }
+    def create(self, validated_data):
+        community = Community.objects.create(
+            creaters =self.context['request'].user, **validated_data
+        )
+        community.members.add(self.context['request'].user)
+        return community
         
         
