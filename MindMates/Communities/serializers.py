c6a802207efe95da6b9615a48322b9f3d71b6b82
Amitstha <shresthaamit273@gmail.com>
2025-04-23 10:17:20 +0545

    Communities Message  started serializer created

--- MindMates/Communities/serializers.py
+++ MindMates/Communities/serializers.py
@@ -22,4 +22,42 @@ class CommunitySerializer(serializers.ModelSerializer):
         community.members.add(self.context['request'].user)
         return community
         
+class CommunityDetailSerializer(serializers.ModelSerializer):
+    creators = UserSerializer()
+    member = UserSerializer()
+    member_count = serializers.SerializerMethodField()
+    is_member = serializers.SerializerMethodField()
+    class Meta:
+        model= Community
+        fields = ['id','name','description','creaters','members','member_count','created_at','images','is_member']
+    def get_member_count(self,obj):
+        return obj.members.count()
+    
+    def get_is_member(self, obj):
+        request =self.context.get('request')
+        if request and request.user.is_authenticated:
+            return obj.member.filter(id = request.user.id).exists()
+        return False
+    
+class JoinLeaveSerializer(serializers.Serializer):
+    user_id = serializers.IntegerField()
+    def validate_user_id(self, value):
+        try:
+            User.objects.get(id=value)
+        except User.DoesNotExist:
+            raise serializers.ValidationError("User does not exist")
+        return value
+
+class CommunityMessageSerializer(serializers.ModelSerializer):
+    sender = UserSerializer()
+    sender_id = serializers.IntegerField(write_only=True, required =False)
+    community_id = serializers.IntegerField(write_only=True, required=False)
+    class Meta:
+        model = CommunityMessage
+        fields = ['id','community','community_id','sender','sender_id','content','file', 'created_at', 'is_edited', 'is_deleted']
+        read_only_fields = ['created_at', 'is_edited', 'is_deleted']
         
+    def create(self, validated_data):
+        validated_data.pop('sender_id', None)
+        validated_data.pop('community_id', None)
+        return super().create(validated_data)
